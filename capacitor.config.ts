d953bdae3653fb2938ba49b05155e3b6cd2e6fa4
vlbr99 <veliborminic99@gmail.com>
2026-08-11 21:27:13 +0200

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mckit.app',
+  appName: 'mckit',
+  webDir: 'dist'
+};
+
+export default config;
