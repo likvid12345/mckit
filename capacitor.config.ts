6b27faf038ede654278b160c1d5ab3eaa01ae6f1
vlbr99 <veliborminic99@gmail.com>
2026-08-12 22:25:00 +0200

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mckit.app',
-  appName: 'mckit',
-  webDir: 'dist'
+	appId: 'com.mckit.app',
+	appName: 'mckit',
+	webDir: 'build'
 };
 
 export default config;
